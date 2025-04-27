99d9b9ef4aea6b567496a5c4cb50952fccaa4730
Silas HAKUZWIMANA <hakusilas@gmail.com>
2025-04-27 10:41:03 +0200

    adding configuration file

---
+++ includes/config.php
@@ -0,0 +1,10 @@
+<?php
+// includes/config.php
+
+// Set the default timezone
+date_default_timezone_set('Africa/Kigali');
+
+// App constants
+define('APP_NAME', 'Mass Reader Scheduler');
+define('BASE_URL', 'https://stbasile.ct.ws/');
+?>
